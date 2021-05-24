e981b582bb6ad67dce80732d0ce07c6159d950c9
Traci <traci.face@gmail.com>
2021-05-23 21:44:53 -0400

    category routes fixed

--- models/Product.js
+++ models/Product.js
@@ -6,18 +6,18 @@ class Product extends Model {}
 Product.init(
   {
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
   },
   product_name: {
       type: DataTypes.STRING,
       allowNull: false
   },
   price: {
-      type: DataTypes.DECIMAL(10,2),
-      allowNull: false,
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
       validate: {
           isDecimal: true
       }
